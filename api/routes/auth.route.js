d3fe58cd7c097203f4b5ea38185f4ff9076b3c94
vipulkandal <vipulkandal.gmail.com>
2025-05-22 13:58:59 +0530

    signin API

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,13 +1,14 @@
 import express from "express";
 import {
-  // google, signOut, signin,
+  // google, signOut,
+  signin,
   signup,
 } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
-// router.post("/signin", signin);
+router.post("/signin", signin);
 // router.post("/google", google);
 // router.get("/signout", signOut);
 
